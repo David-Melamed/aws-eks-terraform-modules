d0536994e519247dbf427b4a8d22e2fc040c2f53
David-Melamed <davidmelamed269@gmail.com>
2024-01-09 16:38:04 +0200

    tf files for cluster creation

---
+++ main.tf
@@ -0,0 +1,31 @@
+###### root/main.tf
+/*
+module "eks" {
+  source                  = "./modules/eks"
+  aws_public_subnet       = module.vpc.aws_public_subnet
+  vpc_id                  = module.vpc.vpc_id
+  cluster_name            = "eks-lab-cluster-${random_string.suffix.result}"
+  endpoint_public_access  = true
+  endpoint_private_access = false
+  public_access_cidrs     = ["0.0.0.0/0"]
+  node_group_name         = "ng-${random_string.suffix.result}"
+  scaling_desired_size    = 1
+  scaling_max_size        = 1
+  scaling_min_size        = 1
+  instance_types          = ["t3.nano"]
+  key_pair                = "DavidKeyPair"
+}
+
+module "vpc" {
+  source                  = "./modules/vpc"
+  tags                    = "eks-lab"
+  instance_tenancy        = "default"
+  vpc_cidr                = "10.0.0.0/16"
+  access_ip               = "0.0.0.0/0"
+  public_sn_count         = 2
+  public_cidrs            = ["10.0.1.0/24", "10.0.2.0/24"]
+  map_public_ip_on_launch = true
+  rt_route_cidr_block     = "0.0.0.0/0"
+
+}
+*/
