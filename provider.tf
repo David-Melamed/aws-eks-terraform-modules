d98122c9075fe960f54be4e75a24e3bcef815e1d
David-Melamed <davidmelamed269@gmail.com>
2024-07-25 12:36:34 +0300

    add cluster authentication

--- provider.tf
+++ provider.tf
@@ -3,8 +3,19 @@ provider "aws" {
 }
 
 provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.auth.token
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    exec {
+        api_version = "client.authentication.k8s.io/v1alpha1"
+        args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+        command     = "aws"
+     }
 }
 
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    token                  = data.aws_eks_cluster_auth.auth.token
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  }
+}
