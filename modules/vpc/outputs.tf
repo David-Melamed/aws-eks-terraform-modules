93ec4a92796f1a940f05967ea55fcbdb73aaabd4
David-Melamed <davidmelamed269@gmail.com>
2024-08-01 18:50:08 +0300

    add dependency to argo_login resource on eks cluster creation, and add data resource to calculate argocd LB hostname to print it as an output

--- modules/vpc/outputs.tf
+++
@@ -1,8 +0,0 @@
-###### vpc/outputs.tf 
-output "aws_public_subnet" {
-  value = aws_subnet.public_ekslab_subnet.*.id
-}
-
-output "vpc_id" {
-  value = aws_vpc.ekslab.id
-}
--- modules/vpc/vpc.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_vpc" "ekslab" {
-  cidr_block       = var.vpc_cidr 
-  instance_tenancy = var.instance_tenancy
-  tags = {
-    Name = var.tags
-  }
-}
