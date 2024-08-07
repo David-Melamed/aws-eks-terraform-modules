8c57891572c42caff8ce174856230fea6f3f8037
David-Melamed <davidmelamed269@gmail.com>
2024-08-07 17:02:16 +0300

    Disable force destroy

--- state.tf
+++ state.tf
@@ -8,10 +8,10 @@ module "terraform_state_backend" {
   name           = "terraform"
   attributes     = ["state"]
   sse_encryption = "AES256"
-
+  
   terraform_backend_config_file_path = "."
   terraform_backend_config_file_name = "backend.tf"
-  force_destroy                      = true
+  force_destroy                      = false
 
   # S3 Bucket configuration
   s3_bucket_name = local.bucket_name
@@ -19,11 +19,11 @@ module "terraform_state_backend" {
   # DynamoDB Table configuration
   dynamodb_enabled = true
   dynamodb_table_name = local.dynamodb_tfstate_table_name
-  deletion_protection_enabled = false #Should be set to true in production
+  deletion_protection_enabled = true
 
   # Additional tags
   tags = {
     Name        = local.cluster_name
     Environment = local.environment
   }
-} 
+}
